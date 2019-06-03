d6e57dd1bcac11af45332bc4dbd9f73320aa59a9
asadkhan20 <asadkhan20@gmail.com>
2019-06-03 18:05:42 -0400

    Add files via upload

---
+++ src/components/seachbar.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+class SearchBar extends React.Component {
+
+    constructor(props){
+        super(props)
+
+        this.state ={ input : ''}
+    }
+
+    render() {
+        return(
+            <div className='search-bar'>
+             <input 
+             value = {this.state.input}
+             onChange={ (event) => this.onInputChange(event.target.value) } 
+             placeholder=' Search'
+             />
+            </div>
+        )
+    }
+
+    onInputChange(input){
+        this.setState({input})
+        this.props.onSearchInput(input)
+    }
+}
+
+export default SearchBar
